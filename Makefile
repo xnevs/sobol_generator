86a52feff4fb7727e8aa5c5407355bf4d00da0cd
Sven Cerk <sven.cerk@gmail.com>
2017-07-15 13:09:10 +0200

    simplify examples

--- Makefile
+++ Makefile
@@ -1,28 +1,8 @@
 MPICXX=mpic++
 CXX_FLAGS=-std=c++14 -O2 -march=native -pedantic -Wall -Wextra -Wno-unused
 
-all: integrate generate evaluate generate10 generate20 generate30 generate40 generate50
+all: example
 
-integrate: examples/integrate.cpp sobol_generator/sobol_generator.hpp sobol_generator/type_to_mpi.hpp
-	$(MPICXX) $(CXX_FLAGS) -o integrate examples/integrate.cpp
+example: example.cpp sobol_generator/sobol_generator.hpp sobol_generator/type_to_mpi.hpp
+	$(MPICXX) -o example example.cpp
 	
-generate: examples/generate.cpp sobol_generator/sobol_generator.hpp sobol_generator/type_to_mpi.hpp
-	$(MPICXX) $(CXX_FLAGS) -o generate examples/generate.cpp
-
-evaluate: examples/evaluate.cpp sobol_generator/sobol_generator.hpp sobol_generator/type_to_mpi.hpp
-	$(MPICXX) $(CXX_FLAGS) -o evaluate examples/evaluate.cpp
-	
-generate10: examples/generate10.cpp sobol_generator/sobol_generator.hpp sobol_generator/type_to_mpi.hpp
-	$(MPICXX) $(CXX_FLAGS) -o generate10 examples/generate10.cpp
-
-generate20: examples/generate20.cpp sobol_generator/sobol_generator.hpp sobol_generator/type_to_mpi.hpp
-	$(MPICXX) $(CXX_FLAGS) -o generate20 examples/generate20.cpp
-
-generate30: examples/generate30.cpp sobol_generator/sobol_generator.hpp sobol_generator/type_to_mpi.hpp
-	$(MPICXX) $(CXX_FLAGS) -o generate30 examples/generate30.cpp
-
-generate40: examples/generate40.cpp sobol_generator/sobol_generator.hpp sobol_generator/type_to_mpi.hpp
-	$(MPICXX) $(CXX_FLAGS) -o generate40 examples/generate40.cpp
-
-generate50: examples/generate50.cpp sobol_generator/sobol_generator.hpp sobol_generator/type_to_mpi.hpp
-	$(MPICXX) $(CXX_FLAGS) -o generate50 examples/generate50.cpp
--- example.cpp
+++ example.cpp
@@ -1,18 +1,25 @@
-#include <cstdio>
+/*
+ * An example of Monte Carlo integration using the
+ * sobol_generator quasi-ranodm number generator.
+ * 
+ * We integrate the function f(x,y) = sin(x) * y^2
+ * on the unit square [0,1]x[0,1].
+ */
+
+#include <iostream>
 #include <vector>
 #include <cmath>
 
 #include <mpi.h>
 
-#include "../sobol_generator/sobol_generator.hpp"
-
-using namespace std;
+#include "sobol_generator/sobol_generator.hpp"
 
 double f(double x, double y) {
-    return sin(x) * pow(y,2);
+    return std::sin(x) * std::pow(y,2);
 }
 
 int main(int argc, char *argv[]) {
+    // initialize MPI
     MPI_Init(&argc, &argv);
 
     int comm_size;
@@ -20,20 +27,28 @@ int main(int argc, char *argv[]) {
     int comm_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
 
-    sobol::sobol_generator<4> sg(argv[1], MPI_COMM_WORLD);
-    double y[4];
+    // create a sobol_generator object
+    sobol::sobol_generator<2> sg(argv[1], MPI_COMM_WORLD);
 
-    int n = 20000000;
+    // prepare an array for the generated vectors
+    double y[2];
 
+    // the number of samples
+    int n = 2000000;
+
+    // Monte Carlo integration
     double sum = 0;
     for(int i=0; i<n; ++i) {
         sg.generate(y);
         sum += f(y[0],y[1]);
     }
 
+    // accumulate the partial sums of all the
+    // processes at the process with rank 0
+    // and output the result at process 0
     if(comm_rank == 0) {
         MPI_Reduce(MPI_IN_PLACE,&sum,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
-        printf("%.16lf\n", sum/(comm_size*n));
+        std::cout << sum / (comm_size*n) << std::endl;
     } else {
         MPI_Reduce(&sum,&sum,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
     }
